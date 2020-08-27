4d4a9e0fab1419e0530a4841a1002dec1847d823
Beaublom4 <Beaublom4@gmail.com>
2020-08-28 01:20:24 +0200

    shop loesoe

--- Tower Defence/Assets/Beau/Script/PlayerMovement.cs
+++ Tower Defence/Assets/Beau/Script/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
@@ -8,11 +9,41 @@ public class PlayerMovement : MonoBehaviour
     public float walkingSpeed;
     float moveFb, moveLr;
 
+    public GameObject playerCam, topViewCam;
+
+    bool canMove = false;
+
     private void Update()
     {
+        if(canMove == true)
+        {
         moveFb = Input.GetAxis("Vertical") * walkingSpeed * Time.deltaTime;
         moveLr = Input.GetAxis("Horizontal") * (walkingSpeed / 2) * Time.deltaTime;
 
         transform.Translate(moveLr, 0, moveFb);
+        }
+        //mag later we ↓
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if(playerCam.activeSelf == true)
+            {
+                playerCam.SetActive(false);
+                topViewCam.SetActive(true);
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                canMove = false;
+            }
+            else if(topViewCam.activeSelf == true)
+            {
+                topViewCam.SetActive(false);
+                playerCam.SetActive(true);
+
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                canMove = true;
+            }
+        }
+        //↑
     }
 }
