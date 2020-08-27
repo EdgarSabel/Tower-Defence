860ac264039776ba11468fc5a83eac0773cadbfc
Beaublom4 <Beaublom4@gmail.com>
2020-08-28 00:00:33 +0200

    Player First person Movement

---
+++ Tower Defence/Assets/Beau/Script/CamLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLook : MonoBehaviour
+{
+    public float sensetivity;
+    public GameObject playerObj;
+
+    float mouseX, MouseY;
+    float xRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensetivity * Time.deltaTime;
+        MouseY = Input.GetAxis("Mouse Y") * sensetivity * Time.deltaTime;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -80, 80);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerObj.transform.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
---
+++ Tower Defence/Assets/Beau/Script/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkingSpeed;
+    float moveFb, moveLr;
+
+    private void Update()
+    {
+        moveFb = Input.GetAxis("Vertical") * walkingSpeed * Time.deltaTime;
+        moveLr = Input.GetAxis("Horizontal") * (walkingSpeed / 2) * Time.deltaTime;
+
+        transform.Translate(moveLr, 0, moveFb);
+    }
+}
