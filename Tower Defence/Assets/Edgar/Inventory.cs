920b3cbcd178f7dc5ab35609c6f271f5297dfc64
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2020-08-29 15:12:25 +0200

    Turret placement update

--- Tower Defence/Assets/Edgar/Inventory.cs
+++ Tower Defence/Assets/Edgar/Inventory.cs
@@ -4,31 +4,56 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public GameObject[] turrets;
+    public float range;
+    public GameObject cam;
     public float xp1, xp2, xp3;
+    private Vector3 offset = new Vector3(0, (float)0.5, 0);
 
     private void Start()
     {
-        
+        cam = GameObject.Find("Camera");
     }
     private void Update()
     {
         if (Input.GetButtonDown("Slot2") && turrets[0] != null)
         {
-            turrets[0].gameObject.SetActive(true);
-            turrets[0].gameObject.transform.position = transform.position;
-            turrets[0] = null;
+            RaycastHit hit;
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+            {
+                if (hit.transform.gameObject.tag == "Ground")
+                {
+                    turrets[0].gameObject.SetActive(true);
+                    turrets[0].gameObject.transform.position = hit.point + offset;
+                    turrets[0] = null;
+                }
+            }
+
         }
         if (Input.GetButtonDown("Slot3") && turrets[1] != null)
         {
-            turrets[1].gameObject.SetActive(true);
-            turrets[1].gameObject.transform.position = transform.position;
-            turrets[1] = null;
+            RaycastHit hit;
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+            {
+                if (hit.transform.gameObject.tag == "Ground")
+                {
+                    turrets[1].gameObject.SetActive(true);
+                    turrets[1].gameObject.transform.position = hit.point + offset;
+                    turrets[1] = null;
+                }
+            }
         }
         if (Input.GetButtonDown("Slot4") && turrets[2] != null)
         {
-            turrets[2].gameObject.SetActive(true);
-            turrets[2].gameObject.transform.position = transform.position;
-            turrets[2] = null;
+            RaycastHit hit;
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+            {
+                if (hit.transform.gameObject.tag == "Ground")
+                {
+                    turrets[2].gameObject.SetActive(true);
+                    turrets[2].gameObject.transform.position = hit.point + offset;
+                    turrets[2] = null;
+                }
+            }
         }
     }
 }
