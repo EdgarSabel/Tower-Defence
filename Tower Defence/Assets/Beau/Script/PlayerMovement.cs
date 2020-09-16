5606300a342d6dbbde14bb33085bf8030ac545b9
Beaublom4 <Beaublom4@gmail.com>
2020-09-16 07:39:44 +0200

    Wat Geluidjes

--- Tower Defence/Assets/Beau/Script/PlayerMovement.cs
+++ Tower Defence/Assets/Beau/Script/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -12,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     public GameObject playerCam;
     Rigidbody playerRg;
     [HideInInspector] public bool canMove = true;
+
+    public float footstepTimer = .5f;
+    public bool isWalking;
 
     [System.Serializable]
     public class Sounds
@@ -29,14 +33,34 @@ public class PlayerMovement : MonoBehaviour
         moveFb = Input.GetAxis("Vertical") * walkingSpeed * Time.deltaTime;
         moveLr = Input.GetAxis("Horizontal") * (walkingSpeed /2) * Time.deltaTime;
 
+        if(moveFb != 0 || moveLr != 0)
+        {
+            isWalking = true;
+        }
+        else if(moveFb != 1 || moveLr != 1)
+        {
+            isWalking = false;
+            footstepTimer = .5f;
+        }
         if(canMove == true) 
             { 
             transform.Translate(moveLr, 0, moveFb);
 
             if (Input.GetButtonDown("Jump") && isGrounded == true)
             {
+                //sounds.jumpSound.Play();
                 playerRg.velocity = new Vector3(0, jumpHight, 0);
             }
+        }
+
+        if(footstepTimer >= -0.0000001 && isWalking == true)
+        {
+            footstepTimer -= Time.deltaTime;
+        }
+        else if(footstepTimer <= -0.000001)
+        {
+            sounds.walkingSound.Play();
+            footstepTimer = .5f;
         }
     }
 }
