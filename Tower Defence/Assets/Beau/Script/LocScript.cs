31e4bec75b9f93d60a80b0275af980e243926a0c
Beaublom4 <Beaublom4@gmail.com>
2020-09-16 15:15:28 +0200

    enemies draaien niet meer dacht ik...

--- Tower Defence/Assets/Beau/Script/LocScript.cs
+++ Tower Defence/Assets/Beau/Script/LocScript.cs
@@ -4,6 +4,7 @@ public class LocScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        print("Test");
         if(other.gameObject.tag == "Enemy")
         {
             other.GetComponent<Enemy>().SetNextLoc();
