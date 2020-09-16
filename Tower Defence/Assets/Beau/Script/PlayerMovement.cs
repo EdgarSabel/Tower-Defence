4f03d65215b2554b1c9be7e1848963a5cb6e1e3d
Beaublom4 <Beaublom4@gmail.com>
2020-09-16 08:22:34 +0200

    FootstepSound

--- Tower Defence/Assets/Beau/Script/PlayerMovement.cs
+++ Tower Defence/Assets/Beau/Script/PlayerMovement.cs
@@ -15,7 +15,8 @@ public class PlayerMovement : MonoBehaviour
     [HideInInspector] public bool canMove = true;
 
     public float footstepTimer = .5f;
-    public bool isWalking;
+    public bool isWalkingFb, isWalkingLr, isStraving;
+    float footstepBaseVolume, footstepBasePitch;
 
     [System.Serializable]
     public class Sounds
@@ -27,21 +28,39 @@ public class PlayerMovement : MonoBehaviour
     {
         canMove = true;
         playerRg = this.GetComponent<Rigidbody>();
+        footstepBaseVolume = sounds.walkingSound.volume;
+        footstepBasePitch = sounds.walkingSound.pitch;
     }
     private void Update()
     {
         moveFb = Input.GetAxis("Vertical") * walkingSpeed * Time.deltaTime;
         moveLr = Input.GetAxis("Horizontal") * (walkingSpeed /2) * Time.deltaTime;
 
-        if(moveFb != 0 || moveLr != 0)
+        if(moveFb != 0 && moveLr == 0)
         {
-            isWalking = true;
+            isStraving = false;
+            isWalkingFb = true;
+            isWalkingLr = false;
         }
-        else if(moveFb != 1 || moveLr != 1)
+        else if (moveLr != 0 && moveFb == 0)
         {
-            isWalking = false;
-            footstepTimer = .5f;
+            isStraving = false;
+            isWalkingFb = false;
+            isWalkingLr = true;
         }
+        else if (moveFb != 0 && moveLr != 0)
+        {
+            isStraving = true;
+            isWalkingFb = false;
+            isWalkingLr = false;
+        }
+        else if(moveFb != 1 && moveLr != 1)
+        {
+            isStraving = false;
+            isWalkingFb = false;
+            isWalkingLr = false;
+        }
+
         if(canMove == true) 
             { 
             transform.Translate(moveLr, 0, moveFb);
@@ -53,14 +72,27 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if(footstepTimer >= -0.0000001 && isWalking == true)
+        if(footstepTimer >= -0.0000001 && (isWalkingFb == true || isWalkingLr == true || isStraving == true))
         {
             footstepTimer -= Time.deltaTime;
         }
         else if(footstepTimer <= -0.000001)
         {
             sounds.walkingSound.Play();
-            footstepTimer = .5f;
+            sounds.walkingSound.volume = Random.Range(footstepBaseVolume - .05f, footstepBaseVolume + .05f);
+            sounds.walkingSound.pitch = Random.Range(footstepBasePitch - .2f, footstepBasePitch + .2f);
+            if (isWalkingFb == true)
+            {
+                footstepTimer = .5f;
+            }
+            else if(isWalkingLr == true)
+            {
+                footstepTimer = .75f;
+            }
+            else if(isStraving == true)
+            {
+                footstepTimer = .38f;
+            }
         }
     }
 }
