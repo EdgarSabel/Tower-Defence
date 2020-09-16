34f0d0804b341fe5e933fdbe95b1fb4bd69e99f7
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2020-09-16 13:25:54 +0200

    turret upgrades (no shop interaction)

--- Tower Defence/Assets/Edgar/Turret.cs
+++ Tower Defence/Assets/Edgar/Turret.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     [System.Serializable]
+    public class LevelOptions
+    {
+        public Stats[] stats;
+    }
+    [System.Serializable]
+    public class Stats
+    {
+        public int damage;
+        public float fireRate, radius, xp, nextLvlXp;
+    }
+    [System.Serializable]
     public class Sounds
     {
         public AudioSource shootSound;
         [HideInInspector] public float shootSoundVolume, shootSoundPitch;
     }
     public int damage, slot;
-    public float fireRate, radius;
+    public float fireRate, radius, levelSpeed, nextLvlXp;
     public new SphereCollider collider;
     [HideInInspector] public GameObject target;
-    [HideInInspector] public float longestDist;
+    [HideInInspector] public float longestDist, xp;
     [HideInInspector]public float standardFireRate;
     [HideInInspector] public float lastShotTime = float.MinValue;
+    [HideInInspector] public int turretLevel;
     public Sounds sounds;
+    public LevelOptions levelStats;
+    private GameObject turretSpawned;
     void Start()
     {
         longestDist = 0;
@@ -56,6 +70,7 @@ public class Turret : MonoBehaviour
             lastShotTime = Time.time;
             Fire();
             ShootSound();
+            EarnXP();
         }
         WipeTarget();
     }
@@ -77,5 +92,41 @@ public class Turret : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         WipeTarget();
+    }
+
+    public void EarnXP()
+    {
+        xp += levelSpeed;
+        //update xp bar here :)
+        if (xp >= nextLvlXp)
+        {
+            //show level up available
+        }
+    }
+
+    public void StatRefresh()
+    {
+        damage = levelStats.stats[turretLevel].damage;
+        fireRate = levelStats.stats[turretLevel].fireRate;
+        radius = levelStats.stats[turretLevel].radius;
+        nextLvlXp = levelStats.stats[turretLevel].nextLvlXp;
+    }
+
+    public void LevelUp()
+    {
+        turretLevel += 1;
+        damage = levelStats.stats[turretLevel].damage;
+        fireRate = levelStats.stats[turretLevel].fireRate;
+        radius = levelStats.stats[turretLevel].radius;
+        nextLvlXp = levelStats.stats[turretLevel].nextLvlXp;
+    }
+
+    //function for shop purchase :O
+    public void switchType(GameObject newTurret, int level)
+    {
+        turretSpawned = Instantiate(newTurret, transform.position, transform.rotation);
+        turretSpawned.GetComponent<Turret>().slot = slot;
+        turretSpawned.GetComponent<Turret>().turretLevel = level;
+        turretSpawned.GetComponent<Turret>().StatRefresh();
     }
 }
