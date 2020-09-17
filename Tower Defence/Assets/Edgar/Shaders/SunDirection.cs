00686e489292dd62e333729734f7909abdc0f686
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2020-09-17 11:04:26 +0200

    skybox finished (not perfect)

---
+++ Tower Defence/Assets/Edgar/Shaders/SunDirection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SunDirection : MonoBehaviour
+{
+    public Vector3 _SunDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Shader.SetGlobalVector(_SunDirection, transform.forward);
+    }
+}
