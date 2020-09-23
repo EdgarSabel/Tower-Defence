e4e24ccde7a21426059241bb9150eafb079a1571
gerranR <60340232+gerranR@users.noreply.github.com>
2020-09-23 22:47:20 +0200

    Enemy HP bar

---
+++ Tower Defence/Assets/Gerran/Scripts/EnemyHPBarScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHPBarScript : MonoBehaviour
+{
+
+    public GameObject enemyOBJ, slider;
+    public Slider enemyHPBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyHPBar.value = enemyOBJ.GetComponent<Enemy>().EnemyHealth;
+
+        if(enemyOBJ.GetComponent<Enemy>().EnemyHealth <= 100)
+        {
+            slider.SetActive(true);
+        }
+    }
+}
