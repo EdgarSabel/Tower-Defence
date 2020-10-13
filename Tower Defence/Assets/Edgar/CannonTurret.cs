4395c344d82649d6a227df0ad5eac15329b9c687
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-10-13 15:53:55 +0200

    cannonSmok

--- Tower Defence/Assets/Edgar/CannonTurret.cs
+++ Tower Defence/Assets/Edgar/CannonTurret.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CannonTurret : Turret
 {
+    public ParticleSystem cannonSmoke;
     public GameObject projectile, currentProjectile, spawnPos;
     public float projectileSpeed;
     public override void Fire()
     {
+        cannonSmoke.Play();
         currentProjectile = Instantiate(projectile, spawnPos.transform.position, transform.rotation);
         currentProjectile.GetComponent<Projectile>().speed = projectileSpeed;
         currentProjectile.GetComponent<Projectile>().damage = damage;
