ad65bfa587f4551960dcd5d570c38b8e9ed826a6
gerranR <60340232+gerranR@users.noreply.github.com>
2020-10-13 13:14:35 +0200

    ability cooldown + TankHPbar Fix

---
+++ Tower Defence/Assets/Gerran/Scripts/AbilityCooldown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityCooldown : MonoBehaviour
+{
+    public Image abilityImageCover;
+    public float cooldownTimeMinPerSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        abilityImageCover.rectTransform.sizeDelta = new Vector2(0, 100);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AbilityTimer();
+    }
+    void AbilityTimer()
+    {
+        if (abilityImageCover.rectTransform.sizeDelta.y >= 0.000001)
+        {
+            abilityImageCover.rectTransform.sizeDelta -= new Vector2(0, cooldownTimeMinPerSec * Time.deltaTime);
+        }
+        if (abilityImageCover.rectTransform.sizeDelta.y <= -0.000001)
+        {
+            abilityImageCover.rectTransform.sizeDelta = new Vector2(0, 0);
+        }
+    }
+
+}
