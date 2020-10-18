eee9129455a407bcd05a50fc011e87315863cd18
Beaublom4 <Beaublom4@gmail.com>
2020-10-18 16:40:10 +0200

    FallDamage

---
+++ Tower Defence/Assets/Beau/Script/KillOnFall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class KillOnFall : MonoBehaviour
+{
+    public GameObject playerObj, spawnLoc;
+    public HealthManager hpManagerScript;
+    public int dmgOnFall;
+    private void OnTriggerEnter(Collider other)
+    {
+        playerObj.transform.position = spawnLoc.transform.position;
+        hpManagerScript.GetDamagedByEnemy(dmgOnFall);
+    }
+}
