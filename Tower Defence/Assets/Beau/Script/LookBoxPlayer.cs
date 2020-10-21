9ceaf15801bad73beb30db47e0ed2e03953bb479
Beaublom4 <Beaublom4@gmail.com>
2020-10-21 10:27:19 +0200

    Shopdingen

--- Tower Defence/Assets/Beau/Script/LookBoxPlayer.cs
+++ Tower Defence/Assets/Beau/Script/LookBoxPlayer.cs
@@ -2,19 +2,11 @@
 
 public class LookBoxPlayer : MonoBehaviour
 {
-    public GameObject shopPanel, turretPanel, hudPanel, cam;
+    public GameObject turretPanel, hudPanel, cam;
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.tag == "Shop" || other.gameObject.tag == "Turret")
         {
-            if(shopPanel.activeSelf == true)
-            {
-                shopPanel.SetActive(false);
-                hudPanel.SetActive(true);
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-                cam.GetComponent<CamLook>().canMove = true;
-            }
             if(turretPanel.activeSelf == true)
             {
                 turretPanel.SetActive(false);
