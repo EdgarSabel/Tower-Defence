14b90a3869d701095ecb94ca8b561e28ebb9cf4a
gerranR <60340232+gerranR@users.noreply.github.com>
2020-10-22 11:03:10 +0200

    turretloadout

---
+++ Tower Defence/Assets/Gerran/Scripts/TurretLoadout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLoadout : MonoBehaviour
+{
+    public GameObject emptyObj;
+    public int[] turrets;
+    public GameObject[] prefabs; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DontDestroyOnLoad(emptyObj);
+
+        Camera.main.GetComponentInParent<Inventory>().turrets[0] = prefabs[turrets[0]];
+        Camera.main.GetComponentInParent<Inventory>().turrets[1] = prefabs[turrets[1]];
+        Camera.main.GetComponentInParent<Inventory>().turrets[2] = prefabs[turrets[2]];
+    }
+}
---
+++ Tower Defence/Assets/Gerran/Scripts/TurretPick.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurretPick : MonoBehaviour
+{
+    public int slotNum;
+    public Sprite normal, shotgun, flame, electric, canon;
+    public Image[] slots;
+    public GameObject turrets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectNormal()
+    {
+        turrets.GetComponent<TurretLoadout>().turrets[slotNum] = 0;
+        slots[slotNum].sprite = normal;
+        if(slotNum == 2)
+        {
+            slotNum = 0;
+        }
+
+        else if(slotNum <= 2)
+        {
+            slotNum += 1;
+        }
+    }
+
+    public void SelectShotgun()
+    {
+        turrets.GetComponent<TurretLoadout>().turrets[slotNum] = 1;
+        slots[slotNum].sprite = shotgun;
+        if (slotNum == 2)
+        {
+            slotNum = 0;
+        }
+
+        else if (slotNum <= 2)
+        {
+            slotNum += 1;
+        }
+    }
+    public void SelectFlame()
+    {
+        turrets.GetComponent<TurretLoadout>().turrets[slotNum] = 2;
+        slots[slotNum].sprite = flame;
+        if (slotNum == 2)
+        {
+            slotNum = 0;
+        }
+
+        else if (slotNum <= 2)
+        {
+            slotNum += 1;
+        }
+    }
+
+    public void SelectElectic()
+    {
+        turrets.GetComponent<TurretLoadout>().turrets[slotNum] = 3;
+        slots[slotNum].sprite = electric;
+        if (slotNum == 2)
+        {
+            slotNum = 0;
+        }
+
+        else if (slotNum <= 2)
+        {
+            slotNum += 1;
+        }
+    }
+    public void SelectCanon()
+    {
+        turrets.GetComponent<TurretLoadout>().turrets[slotNum] = 4;
+        slots[slotNum].sprite = canon;
+        if (slotNum == 2)
+        {
+            slotNum = 0;
+        }
+
+        else if (slotNum <= 2)
+        {
+            slotNum += 1;
+        }
+    }
+
+}
