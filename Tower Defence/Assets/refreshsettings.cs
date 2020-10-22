c5883c2fccb03aab114f3507503b8bb2701b08d2
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2020-10-22 09:27:03 +0200

    settings fix

---
+++ Tower Defence/Assets/refreshsettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class refreshsettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OptionsManeger.masterSliderValue = PlayerPrefs.GetFloat("Master");
+        OptionsManeger.musicSliderValue = PlayerPrefs.GetFloat("Music");
+        OptionsManeger.soundSliderValue = PlayerPrefs.GetFloat("Sound");
+        OptionsManeger.sensitivityStatic = PlayerPrefs.GetFloat("Sens");
+    }
+}
